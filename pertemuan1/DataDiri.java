c86dac2af34dda0cc55e7f2eb42cc9a3abe8c39c
SeptiadiHabib <125534660+SeptiadiHabib@users.noreply.github.com>
2023-02-17 17:51:55 +0700

    Add files via upload

---
+++ pertemuan1/DataDiri.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author RIVAY
+ */
+public class DataDiri {
+    
+    public static void main(String[] args){
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        
+        nama = "Habib";
+        alamat = "Sukaraya";
+        usia = 27;
+        tinggi = 176.6;        
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia + " Tahun");
+        System.out.println("Tinggi Badan: " + tinggi + " cm");
+        
+        
+    }
+}
---
+++ pertemuan1/LuasLingkaran.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author RIVAY
+ */
+public class LuasLingkaran {
+    
+    public static void main(String[] args){
+        
+        Double pi, r, luas;
+        
+        r = 10.0;
+        pi = 3.14;
+        
+        luas = pi * r * r;
+        
+        System.out.println("Luas Lingkaran = ");
+        System.out.println(luas);
+        
+        
+    }
+    
+}
